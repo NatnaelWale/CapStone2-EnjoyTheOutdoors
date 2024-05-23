5c1e317ba222b93d5de2b87a1ebc0dc2d3d7d13a
NatnaelWale <nhattyjeje@gmail.com>
2024-05-23 11:45:22 -0400

    Touched up CSS of the national parks page

--- scripts/nationalPark.js
+++ scripts/nationalPark.js
@@ -6,9 +6,10 @@
 const onSearchByLocationBtn = document.getElementById("location");
 const onSearchByParkTypeBtn = document.getElementById("parkType");
 const onDisplayAllBtn = document.getElementById("displayAll");
-const dropdownDisplay  =document.getElementById("dropdownDisplay");
+const dropdownDisplay1  = document.getElementById("dropdownDisplay1");
+const dropdownDisplay2 = document.getElementById("dropdownDisplay2");
+const dropdownDisplay3 = document.getElementById("dropdownDisplay3");
 const resultDisplay = document.getElementById("resultDisplay");
-
 
 
 
@@ -18,29 +19,43 @@ window.onload = () => {
     onDisplayAllBtn.addEventListener('change', populateOptions);
 };
 
+const capitalizeText = text => text.toUpperCase();
 
 function populateOptions() {
-
+    
     //Let's start by clearing previous results
-    dropdownDisplay.innerHTML = "";
+    dropdownDisplay1.innerHTML = "";
+    dropdownDisplay2.innerHTML = "";
+    dropdownDisplay3.innerHTML = "";
     resultDisplay.innerHTML = "";
-
+    
     //Let's now decide which data to populate based on selected radio buttons
     if(onSearchByLocationBtn.checked){
         populateStateList();
-    } else if(onSearchByParkTypeBtn.checked) {
+    } 
+     if (onSearchByParkTypeBtn.checked){
         populateParkTypeList();
-    } else if (onDisplayAllBtn.checked){
+    }
+     if (onDisplayAllBtn.checked){
         populateAllParks();
     }
-
+    if (onSearchByLocationBtn.checked && onSearchByParkTypeBtn.checked) {
+        populateBothStateAndParkType();
+    }
 }
 
 function populateStateList() {
     //Let's create a div that can hold select elements and options
     const selectState = document.createElement("select");
-    selectState.className = "text-center form-control mt-5"
+    selectState.className = "text-center form form-control mt-5 custom-select1";
+    selectState.addEventListener('change', () => populateParksByState(selectState.value));
     
+    // Create a default option
+    const defaultOption = document.createElement("option");
+    defaultOption.textContent = "Select a State";
+    defaultOption.value = " ";
+    selectState.appendChild(defaultOption);
+
     //Let's now create the options for state list
     locationsArray.forEach(state => {
         const stateOption = document.createElement("option");
@@ -48,14 +63,21 @@ function populateStateList() {
         stateOption.value = state;
         selectState.appendChild(stateOption);
     });
-    dropdownDisplay.appendChild(selectState);
+    dropdownDisplay1.appendChild(selectState);
 
 }
 
 function populateParkTypeList() {
        //Let's create a div that can hold select elements and options
        const selectParkType = document.createElement("select");
-       selectParkType.className = "text-center form-control mt-5"
+       selectParkType.className = "text-center form-control mt-5 custom-select2"
+       selectParkType.addEventListener('change', () => populateParksByType(selectParkType.value));
+
+        // Create a default option
+        const defaultOption = document.createElement("option");
+        defaultOption.textContent = "Select a Park Type";
+        defaultOption.value = " ";
+        selectParkType.appendChild(defaultOption);
        
        //Let's now create the options for park types
        parkTypesArray.forEach(parkType => {
@@ -64,7 +86,7 @@ function populateParkTypeList() {
         parkTypeOption.value = parkType;
         selectParkType.appendChild(parkTypeOption);
     });
-    dropdownDisplay.appendChild(selectParkType);
+    dropdownDisplay2.appendChild(selectParkType);
 
 }
 
@@ -73,7 +95,7 @@ function populateAllParks(){
         // Let's create a table to display the result
         const parkTable = document.createElement("table");
         parkTable.className = "table table-striped table-hover border mt-5";
-        dropdownDisplay.appendChild(parkTable);
+        dropdownDisplay3.appendChild(parkTable);
 
         const parkTableHead = document.createElement("thead");
         parkTableHead.className = "table-dark";
@@ -83,7 +105,7 @@ function populateAllParks(){
 
         // Let's create the header row
         const parkTableHeaderRow = document.createElement("tr");
-        const headers = ["Location Name", "Location ID", "Location Address"];
+        const headers = ["Location Name", "Location Address"];
         headers.forEach(text => {
             const header = document.createElement("th");
             header.textContent = text;
@@ -95,21 +117,137 @@ function populateAllParks(){
         //Let's now fill in the data into the table
         nationalParksArray.forEach(park => {
             const parkTableRow = document.createElement("tr");
-            const nameCell = document.createElement("td");
-            nameCell.className = "w-50"
-            nameCell.textContent = park.LocationName;
-            const idCell = document.createElement("td");
-            idCell.className = "w-25"
-            idCell.textContent = park.LocationID;
-            const addressCell = document.createElement("td");
-            addressCell.textContent = park.Address;
 
-            parkTableRow.appendChild(nameCell);
-            parkTableRow.appendChild(idCell);
-            parkTableRow.appendChild(addressCell);
+        //Creating the table data by using a function called createCell
+
+            parkTableRow.appendChild(createCell(park.LocationName));
+            parkTableRow.appendChild(createCell(park.Address));
             parkTableBody.appendChild(parkTableRow);
         });
 
         parkTable.appendChild(parkTableBody);
-        dropdownDisplay.appendChild(parkTable);
+        dropdownDisplay3.appendChild(parkTable);
+}
+
+function populateParksByState(selectedState){
+
+    
+    // Let's clear previous park listings
+    const parkListings = document.getElementById('parkListings');
+    if (parkListings) {
+        parkListings.remove();
+    }
+    
+    selectedState = capitalizeText(selectedState);
+
+    //Let's first filter selected parks 
+    const filteredParks = nationalParksArray.filter(park => capitalizeText(park.State) === selectedState);
+    if (filteredParks.length > 0) {
+        
+        resultDisplay.innerHTML = "";
+        const table = document.createElement("table");
+        table.id = "parkListings";
+        table.className = "table table-striped table-hover border mt-5";
+        const thead = document.createElement("thead");
+        thead.className = "table-dark";
+        const tbody = document.createElement("tbody");
+        tbody.className = "table-group-divider";
+
+        const headerRow = document.createElement("tr");
+        ["Location Name", "Address", "Visit"].forEach(text => {
+            const header = document.createElement("th");
+            header.textContent = text;
+            headerRow.appendChild(header);
+        });
+
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        filteredParks.forEach(park => {
+            const row = document.createElement("tr");
+            row.appendChild(createCell(park.LocationName));
+            row.appendChild(createCell(park.Address));
+            if(park.Visit > 0){
+            row.appendChild(createVisitLink(park.Visit));
+        } else {
+            row.appendChild(createCell(park.Visit));
+        }
+            tbody.appendChild(row);
+        });
+
+        table.appendChild(tbody);
+        resultDisplay.appendChild(table);
+    } else {
+        resultDisplay.innerHTML = `<p class = "mt-5">No parks found in ${selectedState}.</p>`;
+    }
+}
+
+function populateParksByType(selectedParkType) {
+    
+    // Let's clear previous park type listings
+    const parkTypeListings = document.getElementById('parkTypeListings');
+    if (parkTypeListings) {
+        parkTypeListings.remove();
+    }
+
+    selectedParkType = capitalizeText(selectedParkType);
+
+    //Let's first filter selected park types
+    const filteredParkTypes = nationalParksArray.filter(park => capitalizeText(park.LocationName).includes(selectedParkType));           
+    if (filteredParkTypes.length > 0) {
+        
+        resultDisplay.innerHTML = "";
+        const table = document.createElement("table");
+        table.id = "parkTypeListings";
+        table.className = "table table-striped table-hover border mt-5";
+        const thead = document.createElement("thead");
+        thead.className = "table-dark";
+        const tbody = document.createElement("tbody");
+        tbody.className = "table-group-divider";
+
+        const headerRow = document.createElement("tr");
+        ["Location Name", "Address", "Visit"].forEach(text => {
+            const header = document.createElement("th");
+            header.textContent = text;
+            headerRow.appendChild(header);
+        });
+
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        filteredParkTypes.forEach(park => {
+            const row = document.createElement("tr");
+            row.appendChild(createCell(park.LocationName));
+            row.appendChild(createCell(park.Address));
+            row.appendChild(createCell(park.Visit));
+            tbody.appendChild(row);
+        });
+
+        table.appendChild(tbody);
+        resultDisplay.appendChild(table);
+    } else {
+        resultDisplay.innerHTML = `<p class = "mt-5">No parks found in the selected type of ${selectedParkType}.</p>`;
+    }
+}
+
+function createCell(text) {
+    const cell = document.createElement("td");
+    cell.textContent = text;
+    cell.className = "w-25"
+    return cell;
+}
+
+function createVisitLink(text){
+     // Create a cell for the visit link
+     const visitLinkTd = document.createElement('td');
+     visitLinkTd.className = "w-25";
+ 
+     // Create an anchor element for the visit link
+     const visitLink = document.createElement('a');
+     visitLink.textContent = "Click to see more";
+     visitLink.href = text; 
+     visitLink.target = "_blank";
+ 
+     visitLinkTd.appendChild(visitLink);
+     return visitLinkTd;
 }
